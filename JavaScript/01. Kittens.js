2dae6b29fb5e0a3675e424e739c333b005db67ec
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-09-29 23:43:36 +0200

    Update and rename 00 to 01. Kittens.js

---
+++ JavaScript/01. Kittens.js
@@ -0,0 +1,19 @@
+// Narysuj tyle kotów ile zechcesz!
+var rysujKoty = function (ileRazy) {
+  for (var i = 0; i < ileRazy; i++) {
+    console.log(i + " =^.^=");
+  }
+};
+
+rysujKoty(10); 
+
+// 0 =^.^=
+// 1 =^.^=
+// 2 =^.^=
+// 3 =^.^=
+// 4 =^.^=
+// 5 =^.^=
+// 6 =^.^=
+// 7 =^.^=
+// 8 =^.^=
+// 9 =^.^=
